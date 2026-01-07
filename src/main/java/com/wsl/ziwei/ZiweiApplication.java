ceaab4cccb7090d46f9c1a6b69942775269d9d2d
wang_shilong <891705265@qq.com>
2026-01-07 16:32:03 +0800

    11

---
+++ src/main/java/com/wsl/ziwei/ZiweiApplication.java
@@ -0,0 +1,13 @@
+package com.wsl.ziwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiweiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiweiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wsl/ziwei/ZiweiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsl.ziwei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiweiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
